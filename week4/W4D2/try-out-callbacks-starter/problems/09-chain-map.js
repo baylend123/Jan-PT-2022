bb4f0283b9437872725576d0dbc5bb6b1a366fe4
baylend123 <baylendoss12@gmail.com>
2022-02-09 00:27:01 -0600

    End of day week 4 day 4

--- week4/W4D2/try-out-callbacks-starter/problems/09-chain-map.js
+++ week4/W4D2/try-out-callbacks-starter/problems/09-chain-map.js
@@ -40,48 +40,59 @@ all of the given callbacks. In other words, if three callbacks are given then:
 Examples:
 our input is going to be a number,  an array of callbacks(...callbacks)
 our output is going to be a number
+Write a function `chainMap` that accepts a value and any number of callbacks as
+arguments. The function should return the final result of passing the value through
+all of the given callbacks. In other words, if three callbacks are given then:
+
+- the value is given to the first callback
+- the result of the first callback is given to the second callback
+- the result of the second callback is given to the third callback
+- the result of the third callback is the final result
+
 *******************************************************************************/
+// write a function that takes a num and a number of cbs
+// loop over the cbs array 
+// on each iteration we need to change the value into whatever the cb turns it into
+// return num 
+
+
+// let chainMap = (num, ...cbs) => cbs.reduce((accume, ele) =>  ele(accume), num)
+let chainMap = function(){
+
+    // // loop over the cbs array 
+    // cbs.forEach(function(ele){
+    //     num = ele(num)
+    // })
+    // // for (let i = 0;  i < cbs.length; i++)
+    // //     console.log(cbs)
+    // //     num = cbs(num)
+    // // }
+    // return num
+}
 
 
 
-let chainMap = function(num, ...cbs) {
-    // iterate through all of the callbacks 
-    // let myNewValue;
-    console.log(num)
-    for(let i = 0 ; i < cbs.length; i++){
-        // an individual callback 
-        let cb = cbs[i]
-        // showing that we are accessing each callback
-        // console.log(cb)
-        // after each iteration we num is going to equal the result of the number passed through the callback
-        // reassigning num to the return value of passing the current num into the callback 
-        // console.log(num, 'inside the loop')
-        // console.log(i)
-        num = cb(num)
-        // console.log(num, 'after the reassignment' )
-    }
-    // after we have iterated through all of the callbacks and exit the for loop
-    // we are left with the result of num being pass through all of the callbacks 
-    console.log(num, 'finally')
-    return num
+
+
+
+
+let add5 = function(n) {
+    return n + 5;
 };
-// let add5 = function(n) {
-//     return n + 5;
-// };
 
-// let half = function(n) {
-//     return n / 2;
-// };
+let half = function(n) {
+    return n / 2;
+};
 
-// let square = function(n) {
-//     return n * n;
-// };
+let square = function(n) {
+    return n * n;
+};
 
-// // console.log(chainMap(25, add5));                // 30
-// // console.log(chainMap(25, add5, half));          // 15
-// console.log(chainMap(25, add5, half, square));  // 225
-// // console.log(chainMap(4, square, half));         // 8
-// // console.log(chainMap(4, half, square));         // 4
+// console.log(chainMap(25, add5));                // 30
+// console.log(chainMap(25, add5, half));          // 15
+console.log(chainMap(25, add5, half, square));  // 225
+// console.log(chainMap(4, square, half));         // 8
+// console.log(chainMap(4, half, square));         // 4
 
 
 
